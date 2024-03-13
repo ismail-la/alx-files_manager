f3cfcaad0c0f997630bcfd6674d50d34a9a8e9fb
ismail-la <lahbariismail@gmail.com>
2024-03-13 22:05:34 +0000

    0x04. Files manager

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-/* Represents a Redis utils client. */
 
-const { promisify } = require('util');
 const { createClient } = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
@@ -14,28 +13,24 @@ class RedisClient {
     });
   }
 
-// Check connection status of redis client.
   isAlive() {
     return this.connected;
   }
 
-// Search for value associated with given key.
   async get(key) {
-    const Get_Async = promisify(this.client.get).bind(this.client);
-    const value = await Get_Async(key);
-    return value;
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const val = await getAsync(key);
+    return val;
   }
 
-// Adds a value with given key to redis.
-  async set(key, value, dur) {
-    const Set_Async = promisify(this.client.set).bind(this.client);
-    await Set_Async(key, value, 'EX', dur);
+  async set(key, val, dur) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    await setAsync(key, val, 'EX', dur);
   }
 
-// Deletes a value associated with given key from redis.
   async del(key) {
-    const Del_Async = promisify(this.client.del).bind(this.client);
-    await Del_Async(key);
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
   }
 }
 
