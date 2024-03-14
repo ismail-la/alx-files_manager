7c152adf975872ae999b923ec3a858c2c6cc5ac0
ismail-la <lahbariismail@gmail.com>
2024-03-14 02:08:41 +0000

    0x04. Files manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,12 +2,27 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
+  /**
+   * Controller for endpoint GET /status that retrieves
+   * mongodb client and redis client connection status
+   * @typedef {import("express").Request} Request
+   * @typedef {import("express").Response} Response
+   * @param {Request} _req - request object
+   * @param {Response} res  - response object
+   */
   static getStatus(_req, res) {
     if (dbClient.isAlive() && redisClient.isAlive()) {
       res.status(200).json({ redis: true, db: true });
     }
   }
 
+  /**
+   * Controller for endpoint GET /stats that retrieves
+   * count of users and files
+   * @param {Request} _req - Request object
+   * @param {Response} res  - Response object
+   * @param {import("express").NextFunction} next - Next function
+   */
   static async getStats(_req, res, next) {
     try {
       const users = await dbClient.nbUsers();
