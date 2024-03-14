cfa0b34de8c482787e1871f31518b46fb82e35ea
ismail-la <lahbariismail@gmail.com>
2024-03-14 02:16:30 +0000

    0x04. Files manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,37 +1,22 @@
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+#!/usr/bin/node
+
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
 class AppController {
-  /**
-   * Controller for endpoint GET /status that retrieves
-   * mongodb client and redis client connection status
-   * @typedef {import("express").Request} Request
-   * @typedef {import("express").Response} Response
-   * @param {Request} _req - request object
-   * @param {Response} res  - response object
-   */
-  static getStatus(_req, res) {
-    if (dbClient.isAlive() && redisClient.isAlive()) {
-      res.status(200).json({ redis: true, db: true });
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.json({ redis: true, db: true });
+      res.end();
     }
   }
 
-  /**
-   * Controller for endpoint GET /stats that retrieves
-   * count of users and files
-   * @param {Request} _req - Request object
-   * @param {Response} res  - Response object
-   * @param {import("express").NextFunction} next - Next function
-   */
-  static async getStats(_req, res, next) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      res.status(200).json({ users, files });
-    } catch (err) {
-      next(err);
-    }
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.json({ users, files });
+    res.end();
   }
 }
 
-export default AppController;
+module.exports = AppController;
